8a203d2d13a2b9082e5c93da4e324cc66b901154
satyamkumar20025 <satyamkumar6372@gmail.com>
2025-11-27 19:34:48 +0530

    File

---
+++ src/filehandling/ReadFileExample.java
@@ -0,0 +1,29 @@
+package filehandling;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFileExample {
+    public static void main(String[] args) {
+        try {
+            FileReader reader = new FileReader("example.txt");
+            int character;
+            int count=0;
+            // read()--> one character at time read--> integer value--> convert into character
+            character = reader.read();
+            while (character!= -1) {
+            	count++;
+            	System.out.print((char)character);
+            	character = reader.read();
+            }
+            System.out.println();
+            System.out.println("Number of characters:"+count);
+            reader.close();
+			
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/filehandling/WriteFileExample.java
@@ -0,0 +1,23 @@
+package filehandling;
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteFileExample {
+    public static void main(String[] args) {
+        try {
+        	
+            FileWriter writer = new FileWriter("example.txt");
+            writer.write("Hello, this is Satyam.\nWelcome to Java File Handling!!!");
+            writer.write("\nHello, this is Satyam.\nWelcome to Java File Handling!!!");
+            
+            writer.close();
+            System.out.println("Successfully wrote to the file.");
+            
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
